3fa4e625918bafd99eda3021b751cd2f7685959c
Marcel Hecko <maco@blava.net>
2018-05-31 16:39:18 +0200

    testing

--- tkinter-test.py
+++ tkinter-test.py
@@ -2,12 +2,14 @@
 
 import tkinter as tk
 
+
 def request_reading(event):
     outText.config(state=tk.NORMAL)
     outText.insert(tk.END, str(event) + " " + entry.get() + "\n")
     outText.insert(tk.END, str(listbox.curselection()) + "\n")
     outText.config(state=tk.DISABLED)
     return event
+
 
 root = tk.Tk()
 
@@ -27,16 +29,13 @@ outText = tk.Text(root, bd=1, state=tk.DISABLED)
 outText.grid(row=0, column=1, rowspan=2)
 
 trigLabel = tk.Label(root, text="Trigger group")
-trigButton = tk.Button(root,
-    font=("Helvetica", 20),
-    text="Trigger Settings",
-)
-trigButton.bind('<Button-1>', request_reading)
+trigButton = tk.Button(root, font=("Helvetica", 20), text="Trigger Settings")
+trigButton.bind("<Button-1>", request_reading)
 trigButton.grid(row=1, column=0)
 
 horizLabel = tk.Label(root, text="Horizontal group")
 horizButton = tk.Button(root, text="Horizontal settings")
-horizButton.bind('<Button-1>', request_reading)
+horizButton.bind("<Button-1>", request_reading)
 horizButton.grid(row=2, column=0)
 
 root.mainloop()
