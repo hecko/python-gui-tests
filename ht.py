0082dbac4c617f51c92f7e40fe0075ccbdf94399
Marcel Hecko <maco@blava.net>
2018-06-01 09:01:40 +0200

    update test

--- ht.py
+++ ht.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 
+import dateutil.parser
+from dateutil import tz
 import tkinter as tk
 import time
+import pytz
+import requests
+
 
 def update():
-    chLabel1['text'] = time.time()
-    chLabel2['text'] = time.time()
-    chLabel3['text'] = time.time()
-    root.after(1000, update)
+    try:
+        url = "http://lorawan.sk:8000/api/node/kuyqiyvsetnsf43grs8ozumh5fipytqs"
+        n = requests.get(url, timeout=5).json()
+        chLabel2["text"] = str(round(n["keys"][0]["last_value"], 1)) + "°C"
+        chLabel4["text"] = str(int(n["keys"][1]["last_value"])) + "%"
+        last_point = dateutil.parser.parse(n["last_point"])
+        chLabel5["text"] = str(last_point.astimezone(tz.tzlocal()))
+    except Exception as e:
+        print(e)
+    root.after(50000, update)
+
 
 root = tk.Tk()
 
-chLabel1 = tk.Label(root, text="Channel group", font=('Helvetica', 30))
-chLabel1.grid(row=0, column=0)
+chLabel2 = tk.Label(root, text="", font=("Helvetica", 100))
+chLabel2.grid(row=0, column=0)
 
-chLabel2 = tk.Label(root, text="Channel group", font=('Helvetica', 30))
-chLabel2.grid(row=0, column=1)
+chLabel4 = tk.Label(root, text="", font=("Helvetica", 100))
+chLabel4.grid(row=1, column=0)
 
-chLabel3 = tk.Label(root,
-    text="Channel group",
-    height=5,
-    font=('Helvetica', 30))
-chLabel3.grid(row=1, column=0)
-
-chLabel4 = tk.Label(root,
-    text="Channel group",
-    height=5,
-    font=('Helvetica', 30))
-chLabel4.grid(row=1, column=1)
+chLabel5 = tk.Label(root, text="", font=("Helvetica", 20))
+chLabel5.grid(row=2, column=0)
 
 update()
-
 root.mainloop()
-
